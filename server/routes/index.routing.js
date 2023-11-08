40b16b8f79ceeb54ee5a534b65227f90de3012c9
vinhngo1907 <vinhtrungngo1907@gmail.com>
2023-11-08 09:45:40 +0700

    initial commit

--- server/routes/index.routing.js
+++
@@ -1,7 +0,0 @@
-const eventRouter = require("./event.routing");
-const baseUrl = '/api/v1';
-function createRoutes(app){
-	app.use(baseUrl + '/event', eventRouter)
-}
-
-module.exports = createRoutes;
