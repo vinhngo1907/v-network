67da9b7a776db204189992bed0db706908bbd19d
vinhngo1907 <vinhtrungngo1907@gmail.com>
2023-11-08 10:48:57 +0700

    commit

---
+++ cmd.js
@@ -0,0 +1,12 @@
+let {
+    program
+} = require("commander"),
+    File = require("./src/util/File");
+
+program
+    .command('serve')
+    .description('Initialization server')
+    .action(async () => {
+        await File.mkdirForUploadFile();
+    })
+    .parse();
