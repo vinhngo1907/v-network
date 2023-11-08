8c09fd255027b32144b295a8cb78cf940b73c0f0
vinhngo1907 <vinhtrungngo1907@gmail.com>
2023-11-08 14:24:56 +0700

    ( *︾▽︾)

---
+++ view/src/common/fetch.js
@@ -0,0 +1,11 @@
+import Cookies from "universal-cookie";
+import config from 'config';
+import axios from "axios";
+
+async function axiosReq(url, option){
+
+}
+
+export async function resApi(endPoint, option){
+
+}
