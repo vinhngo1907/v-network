63d5fc6878465128537a6d50a5d3b868f6a5956b
vinhngo1907 <vinhtrungngo1907@gmail.com>
2024-02-18 09:28:49 +0700

    commit

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 // import { IdeaModule } from 'src/idea/module';
-import { DatabaseModule } from 'src/database';
+// import { DatabaseModule } from 'src/database';
 import { KafkaModule } from 'src/kafka/kafka.module';
 import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
 // import { HttpErrorFilter } from 'src/shared/http.error.filter';
@@ -9,7 +9,7 @@ import { ConfigService } from '@nestjs/config';
 import { AppConfigService } from 'src/config/appConfigService';
 
 @Module({
-    imports: [DatabaseModule, KafkaModule],
+    imports: [KafkaModule],
     controllers: [],
     providers: [
         ConfigService,
