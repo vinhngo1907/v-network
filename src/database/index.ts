b5229d623d3ff739e2615c2158294db052203dce
vinhngo1907 <vinhtrungngo1907@gmail.com>
2024-02-18 09:15:54 +0700

    commit

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { DatabaseService } from './mongodb/mongoDbDriverConnection';
+import { DatabaseService } from './mongodb/service';
 
 @Module({
   providers: [DatabaseService],
