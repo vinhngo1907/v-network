b38aa8eab3f761a7ee3f9490210300abee88a35e
vinhngo1907 <vinhtrungngo1907@gmail.com>
2024-03-10 21:12:14 +0700

    user input

--- src/modules/base/UserInput.ts
+++ src/modules/base/UserInput.ts
@@ -3,5 +3,9 @@ import Json from "../../utils/ReturnJson";
 import Response from "../../utils/Response";
 
 export abstract class UserInput {
-
+    async isUndefined(data: unknown) {
+        if (Util.isUndefined(data)) {
+            return Json.builder(Response.HTTP_BAD_REQUEST);
+        }
+    }
 }
