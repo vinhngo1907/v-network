3f36bfed70edd55e1bd67258710d212c69720e52
Koo Kuu <vinhtrungngo1907@gmail.com>
2025-01-04 20:03:16 +0700

    clean database

--- packages/server/prisma/migrations/20240601053542_update_schema/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "video" ALTER COLUMN "thumbnail" DROP NOT NULL;
--- packages/server/prisma/migrations/20240601053716_update_schema/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "course" ALTER COLUMN "thumbnail" DROP NOT NULL;
-
--- AlterTable
-ALTER TABLE "video" ALTER COLUMN "videoUrl" DROP NOT NULL;
